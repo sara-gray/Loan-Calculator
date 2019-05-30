84eedd1f84b979e2b2495b37df20a67cd02e01cd
sara-gray <saragrayen@gmail.com>
2019-05-30 14:19:59 +0100

    Added loader

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const yearsRepay = document.querySelector("#years");
 const monthlyPayment = document.querySelector("#monthly-payment");
 const totalPayment = document.querySelector("#total-payment");
 const totalInterest = document.querySelector("#total-interest");
+const loading = document.querySelector("#loading");
+const results = document.querySelector("#results");
 
 // Load all event listeners
 loadEventListeners();
@@ -18,34 +20,51 @@ function loadEventListeners() {
 }
 
 function calculate(e) {
+  results.style.display = 'none';
+  loading.style.display = 'none';
+  
   // parseFloat converts to a decimal
   const principalAmount = parseFloat(loanAmount.value);
   const calcInterest = parseFloat(interestRate.value / 100 / 12);
   const calcPayments = parseFloat(yearsRepay.value * 12);
-
+  
   const x = Math.pow(1 + calcInterest, calcPayments);
   const monthly = (principalAmount * x * calcInterest) / (x - 1);
   if (isFinite(monthly)) {
+    loadingStart();
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calcPayments).toFixed(2);
     totalInterest.value = (monthly * calcPayments - principalAmount).toFixed(2);
+    setTimeout(loadingStop, 2000);
+    
   } else {
     showError("Please check you have entered your numbers correctly?");
   }
-
+  
   e.preventDefault();
 }
 
+function loadingStart() {
+  results.style.display = 'none';
+  loading.style.display = 'block';
+}
+
+function loadingStop() {
+  results.style.display = 'block';
+  loading.style.display = 'none';
+}
+
 function showError(error) {
+  
   const errorDiv = document.createElement("div");
   const card = document.querySelector(".card");
   const heading = document.querySelector(".heading");
   errorDiv.className = "alert alert-danger";
   errorDiv.appendChild(document.createTextNode(error));
-
+  
   // Insert error above heading
   card.insertBefore(errorDiv, heading);
-
+  
   // Clear error after 3 seconds
   setTimeout(clearError, 3000);
 }
